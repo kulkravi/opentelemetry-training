7cac4c010a1351de4ac3d6c479625dc5af7c33cd
Ravindra Kulkarni <ravi@Ravindras-MacBook-Pro.local>
2024-08-27 06:38:05 +0530

    Added Logs module, removed some redundant code etc

--- StartCollector.bash
+++ StartCollector.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker stop collector
-docker rm collector
-docker run -p 4317:4317 -p 4318:4318 -p 8888:8888  -d -v $(pwd)/collector-config.yaml:/etc/otelcol/config.yaml --name collector otel/opentelemetry-collector
+# docker stop collector
+# docker rm collector
+# docker run -p 4317:4317 -p 4318:4318 -p 8888:8888  -d -v $(pwd)/collector-config.yaml:/etc/otelcol/config.yaml --name collector otel/opentelemetry-collector
+kill $(cat collector.pid)
+sleep 2
+./otelcol --config collector-config.yaml > collector.log 2>&1 &
+echo $! > collector.pid
